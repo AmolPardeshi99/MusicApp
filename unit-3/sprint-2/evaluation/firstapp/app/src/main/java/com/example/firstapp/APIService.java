ef993870d7bc47a7eb1d8fc0666feb5fabdd6f4a
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-08-09 15:19:17 +0530

    2nd commit

--- unit-3/sprint-2/evaluation/firstapp/app/src/main/java/com/example/firstapp/APIService.java
+++ unit-3/sprint-2/evaluation/firstapp/app/src/main/java/com/example/firstapp/APIService.java
@@ -9,5 +9,5 @@ import retrofit2.http.Path;
 public interface APIService {
 
     @GET("shivarajp/2cbe00030c04472c9d8ad4b0ec112dbe/raw/c651391e428182f08d60d59e79073f3fcf79b858/nobroker")
-    Call<List<ResponseModel>> getData(@Path("") int id);
+    Call<List<ResponseModel>> getData();
 }
--- unit-3/sprint-2/evaluation/firstapp/app/src/main/java/com/example/firstapp/MainActivity.java
+++ unit-3/sprint-2/evaluation/firstapp/app/src/main/java/com/example/firstapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(MainActivity.this,ItemListActivity.class));
+            MainActivity.this.finish();
+        },3000);
     }
 }
