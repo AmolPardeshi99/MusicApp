db747a7d1195137ffd645d1547ff4722376de40e
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-08-09 17:20:16 +0530

    Added fix me app

--- unit-3/sprint-2/evaluation/firstapp/app/src/main/java/com/example/firstapp/ItemListActivity.java
+++ unit-3/sprint-2/evaluation/firstapp/app/src/main/java/com/example/firstapp/ItemListActivity.java
@@ -1,14 +1,27 @@
 package com.example.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
 public class ItemListActivity extends AppCompatActivity {
-
+    private FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
+        fragmentManager = getSupportFragmentManager();
+        initviews();
     }
+
+    private void initviews() {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        ItemListFragment itemListFragment = new ItemListFragment();
+        transaction.replace(R.id.flcontainer,itemListFragment,"itemlistfrag").commit();
+    }
+
+
 }
