7c31f9a307e983d9ec642cd65630b4dcaaf2bdd9
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-08-09 16:12:06 +0530

    Commit 4:10

--- unit-3/sprint-2/evaluation/firstapp/app/src/main/java/com/example/firstapp/ItemAdapter.java
+++ unit-3/sprint-2/evaluation/firstapp/app/src/main/java/com/example/firstapp/ItemAdapter.java
@@ -1,24 +1,37 @@
 package com.example.firstapp;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ItemAdapter extends RecyclerView.Adapter<ResponseModel> {
+import java.util.List;
+
+public class ItemAdapter extends RecyclerView.Adapter<itemViewHolder> {
+
+    List<ResponseModel> itemList;
+
+    public ItemAdapter(List<ResponseModel> itemList) {
+        this.itemList = itemList;
+    }
+
     @NonNull
     @Override
-    public ResponseModel onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public itemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout,parent,false);
+        return new itemViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull  ResponseModel holder, int position) {
-
+    public void onBindViewHolder(@NonNull itemViewHolder holder, int position) {
+        ResponseModel responseModel = itemList.get(position);
+        holder.setData(responseModel);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return itemList.size();
     }
 }
--- unit-3/sprint-2/evaluation/firstapp/app/src/main/java/com/example/firstapp/ItemListFragment.java
+++ unit-3/sprint-2/evaluation/firstapp/app/src/main/java/com/example/firstapp/ItemListFragment.java
@@ -2,14 +2,31 @@ package com.example.firstapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ItemListFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private List<ResponseModel> responseModelList = new ArrayList<>();
+    private ItemAdapter itemAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -17,4 +34,44 @@ public class ItemListFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_item_list, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView(view);
+        setRecyclerView();
+        callAPI();
+    }
+
+    private void callAPI() {
+        APIService apiService = Network.getInstance().create(APIService.class);
+        Call<List<ResponseModel>> call = apiService.getData();
+        call.enqueue(new Callback<List<ResponseModel>>() {
+            @Override
+            public void onResponse(Call<List<ResponseModel>> call, Response<List<ResponseModel>> response) {
+                if (response.code() == HttpURLConnection.HTTP_OK && response.body()!= null){
+                    responseModelList = response.body();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<ResponseModel>> call, Throwable t) {
+                Toast.makeText(getContext(),"Loading Failed",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initView(View view) {
+        recyclerView = view.findViewById(R.id.recyclerview);
+    }
+
+
+    private void setRecyclerView(){
+        itemAdapter = new ItemAdapter(responseModelList);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setAdapter(itemAdapter);
+        recyclerView.setLayoutManager(linearLayoutManager);
+    }
+
+
 }
