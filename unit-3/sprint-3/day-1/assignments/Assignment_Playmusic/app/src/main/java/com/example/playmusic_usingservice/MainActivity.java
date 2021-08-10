bb07a11ceabce9feb37ad970785ea13b337d8438
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-08-10 19:24:21 +0530

    Play Music on CLicking Start and Stop playing on Click of stop button.

---
+++ unit-3/sprint-3/day-1/assignments/Assignment_Playmusic/app/src/main/java/com/example/playmusic_usingservice/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.playmusic_usingservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnStart, mBtnStop;
+    private Intent intent ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mBtnStart = findViewById(R.id.btnStart);
+        mBtnStop = findViewById(R.id.btnStop);
+        intent = new Intent(this,MusicService.class);
+        mBtnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(intent);
+            }
+        });
+
+        mBtnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(intent);
+
+            }
+        });
+    }
+}
---
+++ unit-3/sprint-3/day-1/assignments/Assignment_Playmusic/app/src/main/java/com/example/playmusic_usingservice/MusicService.java
@@ -0,0 +1,70 @@
+package com.example.playmusic_usingservice;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.IBinder;
+
+import androidx.core.app.NotificationCompat;
+
+import java.util.Objects;
+
+public class MusicService extends Service {
+    private MediaPlayer mediaPlayer;
+    public MusicService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaPlayer = MediaPlayer.create(this,R.raw.shape_of_you);
+        showNotificationAndStartForeGround();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        startMusic();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void startMusic() {
+        mediaPlayer.start();
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+    }
+
+    private void showNotificationAndStartForeGround() {
+        createChannel();
+        NotificationCompat.Builder notificationBuilder = null;
+        notificationBuilder = new NotificationCompat.Builder(this, "CHANNEL_ID")
+                .setContentTitle("MusicService is running ")
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        Notification notification = null;
+        notification = notificationBuilder.build();
+        startForeground(120, notification);
+    }
+
+    public void createChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("CHANNEL_ID", "CHANNEL_NAME", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("Notifications");
+            Objects.requireNonNull(this.getSystemService(NotificationManager.class)).createNotificationChannel(channel);
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
