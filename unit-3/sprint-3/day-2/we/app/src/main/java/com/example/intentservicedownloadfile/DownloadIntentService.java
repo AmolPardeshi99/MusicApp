b90c4316d5aa582e24781103935789b06d1022e1
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-08-11 16:47:35 +0530

    Design an Android app and download a file using Intent Service. Once the file is downloaded update the UI.

--- unit-3/sprint-3/day-2/we/app/src/main/java/com/example/intentservicedownloadfile/DownloadIntentService.java
+++ unit-3/sprint-3/day-2/we/app/src/main/java/com/example/intentservicedownloadfile/DownloadIntentService.java
@@ -3,6 +3,15 @@ package com.example.intentservicedownloadfile;
 import android.app.IntentService;
 import android.content.Intent;
 import android.content.Context;
+import android.content.res.Configuration;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 public class DownloadIntentService extends IntentService {
 
@@ -12,8 +21,47 @@ public class DownloadIntentService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        if (intent != null) {
+        downloadFile();
+    }
 
+    private void downloadFile() {
+        try {
+            File Directory = new File(getFilesDir() + File.separator + "Vogella");
+
+            if (!Directory.exists()) {
+                Directory.mkdir();
+            }
+            File file = new File(Directory, "vogella.html");
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            URL url = new URL("https://www.vogella.com/index.html");
+
+            InputStream inputStream = url.openConnection().getInputStream();
+
+            InputStreamReader reader = new InputStreamReader(inputStream);
+            FileOutputStream writer = new FileOutputStream(file, true);
+            int data = reader.read();
+            while (data != -1) {
+                char ch = (char) data;
+                writer.write(ch);
+                data = reader.read();
+            }
+
+            FileInputStream fileInputStream = new FileInputStream(file);
+            int filedata = fileInputStream.read();
+            StringBuffer stringBuffer = new StringBuffer();
+            while (filedata != -1) {
+                char ch = (char) filedata;
+                stringBuffer = stringBuffer.append(ch);  // mutable in nature
+                filedata = fileInputStream.read();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
+
 }
