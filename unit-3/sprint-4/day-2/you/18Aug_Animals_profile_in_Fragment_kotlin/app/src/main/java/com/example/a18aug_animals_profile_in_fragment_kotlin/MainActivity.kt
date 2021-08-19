337300dccec202a439f1b04acd750eeb73677162
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-08-19 10:15:06 +0530

    Kotlin-Load ten Animals profile in a Fragment - using glide

--- unit-3/sprint-4/day-2/you/18Aug_Animals_profile_in_Fragment_kotlin/app/src/main/java/com/example/a18aug_animals_profile_in_fragment_kotlin/MainActivity.kt
+++ unit-3/sprint-4/day-2/you/18Aug_Animals_profile_in_Fragment_kotlin/app/src/main/java/com/example/a18aug_animals_profile_in_fragment_kotlin/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.a18aug_animals_profile_in_fragment_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.FragmentManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    //private lateinit var fragmentManager: FragmentManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnLoad.setOnClickListener{
+            whenButtonClicked()
+            btnLoad.visibility = View.GONE
+        }
+
     }
+
+    private fun whenButtonClicked() {
+        var fragmentManager = supportFragmentManager
+        val animalFragment = AnimalFragment()
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.flcontainer,animalFragment).commit()
+    }
+
+
 }
