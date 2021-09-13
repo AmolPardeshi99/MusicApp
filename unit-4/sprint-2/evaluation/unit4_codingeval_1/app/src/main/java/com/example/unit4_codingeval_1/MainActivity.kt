b07d069f1964a5bf736a6b7b3414355d97beb353
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-09-13 15:34:58 +0530

    Added DataHandler class and fragments-2 2

--- unit-4/sprint-2/evaluation/unit4_codingeval_1/app/src/main/java/com/example/unit4_codingeval_1/MainActivity.kt
+++ unit-4/sprint-2/evaluation/unit4_codingeval_1/app/src/main/java/com/example/unit4_codingeval_1/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.unit4_codingeval_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
+    lateinit var fragmentManager: FragmentManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fragmentManager = supportFragmentManager
+        setFragment1()
+        setFragment2()
+    }
+
+    private fun setFragment1() {
+        val fragmenttransaction = fragmentManager.beginTransaction()
+        val fragment1 = AddItemFragment()
+        fragmenttransaction.replace(R.id.flContainer1,fragment1).addToBackStack("add item frag").commit()
+    }
+
+    private fun setFragment2() {
+        val fragmenttransaction = fragmentManager.beginTransaction()
+        val fragment2 = SearchItemFragment()
+        fragmenttransaction.replace(R.id.flContainer2,fragment2).addToBackStack("search fragment").commit()
 
     }
+
+
 }
