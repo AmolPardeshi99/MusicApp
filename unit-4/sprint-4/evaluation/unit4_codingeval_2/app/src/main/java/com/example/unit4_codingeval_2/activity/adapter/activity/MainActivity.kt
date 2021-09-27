94c92cff591385c5ee8787976ea21a8dc58a91e2
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-09-27 17:04:00 +0530

    Added recyclerview and api service

--- unit-4/sprint-4/evaluation/unit4_codingeval_2/app/src/main/java/com/example/unit4_codingeval_2/activity/adapter/activity/MainActivity.kt
+++ unit-4/sprint-4/evaluation/unit4_codingeval_2/app/src/main/java/com/example/unit4_codingeval_2/activity/adapter/activity/MainActivity.kt
@@ -2,19 +2,54 @@ package com.example.unit4_codingeval_2.activity.adapter.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.unit4_codingeval_2.R
 import com.example.unit4_codingeval_2.activity.adapter.recyclerview.PizzaAdapter
 import com.example.unit4_codingeval_2.activity.adapter.recyclerview.PizzaModel
+import com.example.unit4_codingeval_2.model.remote.retrofit.APIService
+import com.example.unit4_codingeval_2.model.remote.retrofit.ItemOnClickListener
+import com.example.unit4_codingeval_2.model.remote.retrofit.Network
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),ItemOnClickListener {
 
     lateinit var pizzaAdapter: PizzaAdapter
     lateinit var pizzaList: MutableList<PizzaModel>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        callAPi()
+    }
+
+    private fun callAPi() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val api  = Network.getRetrofit().create(APIService::class.java)
+            val response = api.getAllPizzaData()
+
+            var pizzaModel : PizzaModel = PizzaModel()
+            pizzaModel.desc = response?.description
+            pizzaModel.name = response?.name
+            pizzaModel.prize = 150.0
+
+            pizzaList.add(pizzaModel)
+
+            CoroutineScope(Dispatchers.Main).launch {
+                recyclerview.adapter = PizzaAdapter(this@MainActivity,pizzaList,this@MainActivity)
+                recyclerview.layoutManager = LinearLayoutManager(this@MainActivity)
+            }
+
+        }
+    }
+
+    override fun onItemClicked(pizzaModel: PizzaModel) {
+
     }
 
 
