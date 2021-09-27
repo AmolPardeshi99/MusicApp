5ccccd3d33ba8ee1a66f95d5fcba8ca2d4664e9d
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-09-27 14:07:00 +0530

    Start of Project

---
+++ unit-4/sprint-4/evaluation/unit4_codingeval_2/app/src/main/java/com/example/unit4_codingeval_2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.unit4_codingeval_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
